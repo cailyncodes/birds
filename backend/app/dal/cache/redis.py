a8f32395196452c56e7d427964d8f66267388b36
Cailyn Hansen <cailyn.e.hansen+github@gmail.com>
2026-02-05 19:31:36 -0500

    Debug

--- backend/app/dal/cache/redis.py
+++ backend/app/dal/cache/redis.py
@@ -1,6 +1,5 @@
 import os
 from minject import inject
-from redis import AuthenticationError
 from redis.asyncio import Redis, ConnectionPool
 from redis.typing import EncodableT as RedisEncodableT
 from lib.cache import CacheProvider, EncodableT
@@ -8,7 +7,7 @@ from lib.cache import CacheProvider, EncodableT
 @inject.bind(
     host=os.getenv("REDISHOST"),
     port=os.getenv("REDISPORT"),
-    username=os.getenv("REDISUSER"),
+    username=os.getenv("REDISUSER") or "default",
     password=os.getenv("REDISPASSWORD"),
 
 )
@@ -20,23 +19,13 @@ class RedisCache(CacheProvider):
             username=username,
             password=password,
             socket_timeout=5,
+            protocol=3,
         )
         self.redis = Redis(
             connection_pool=connection_pool,
             decode_responses=True,
         )
-        _ = self.test()
-    
-    async def test(self):
-        try:
-            pong = await self.redis.ping() # type: ignore
-            print(f"Redis connected successfully: {pong}")
-        except AuthenticationError as e:
-            print(f"Authentication Failed: Check if REDISPASSWORD is correct. Error: {e}")
-            raise
-        except ConnectionError as e:
-            print(f"Connection Failed: Is REDISHOST/REDISPORT correct? Error: {e}")
-            raise
+        print(f"Attempting connection to {host}:{port} as user '{username}'...")
 
     async def get(self, key: str):
         return await self.redis.get(key)
